0918af5bd536d2223667a81a5e9649f3fa160c23
David Lopez <wdavidlopez4@gmail.com>
2020-06-27 14:39:30 -0500

    Creacion de la Interfaces de la capa de Dominio

---
+++ src/Monolegal.Domain/Interfaces/IDomain.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monolegal.Domain.Interfaces
+{
+    public interface IDomain<T>
+    {
+        public Task<bool> AddObjet(T objeto);
+
+        public Task<T> GetObjet(string id);
+
+        public Task<IEnumerable<T>> GetAllObjet();
+    }
+}
