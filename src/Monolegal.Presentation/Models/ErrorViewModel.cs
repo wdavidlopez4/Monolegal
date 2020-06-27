be810e5d4c8da0d23ffc00b6c66b56a51fb7d9ac
David Lopez <wdavidlopez4@gmail.com>
2020-06-27 14:14:02 -0500

    Primera iteracion para arquitectar el software

---
+++ src/Monolegal.Presentation/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Monolegal.Presentation.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
