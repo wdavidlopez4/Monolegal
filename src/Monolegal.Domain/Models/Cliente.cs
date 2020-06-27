aea7755f635ca77408c8aa3ce39bf97479c7d09a
David Lopez <wdavidlopez4@gmail.com>
2020-06-27 14:27:16 -0500

    Se crearon los modelos

---
+++ src/Monolegal.Domain/Models/Cliente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Monolegal.Domain.Models
+{
+    public class Cliente
+    {
+        public string Nombre { get; set; }
+
+        public string Ciudad { get; set; }
+
+        public int NIT { get; set; }
+    }
+}
---
+++ src/Monolegal.Domain/Models/Factura.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Monolegal.Domain.Models
+{
+    public class Factura
+    {
+        public Guid Id { get; set; }
+
+        public double TotalFactura { get; set; }
+
+        public double SubTotal { get; set; }
+
+        public double IVA { get; set;  }
+
+        public double Retencion { get; set; }
+
+        public DateTime FechaDeCreacion { get; set; }
+
+        public string Estado { get; set; }
+
+        public bool Pagada { get; set; }
+
+        public DateTime FechaDePago { get; set; }
+    }
+}
